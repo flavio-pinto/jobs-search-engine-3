ebe64cf19abf4aa3907aaf56cf9d9161a3357bef
Flavio Pinto <flaviopnt@hotmail.it>
2022-09-14 00:49:26 +0200

    Persistency and security

--- src/components/CompanySearchResults.jsx
+++ src/components/CompanySearchResults.jsx
@@ -1,28 +1,32 @@
-import { useEffect } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import Job from './Job'
-import { useParams } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { getCompanyJobsAction, SET_COMPANY_NAME } from '../redux/actions'
+import { useEffect } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import Job from "./Job";
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getCompanyJobsAction, SET_COMPANY_NAME } from "../redux/actions";
 
 const CompanySearchResults = () => {
   const params = useParams();
   const dispatch = useDispatch();
-  const jobsFetched = useSelector((state) => state.jobs.companyJobs)
-  const companyName = useSelector(state => state.jobs.companyName)
+  const navigate = useNavigate();
+  const jobsFetched = useSelector((state) => state.jobs.companyJobs);
+  const companyName = useSelector((state) => state.jobs.companyName);
 
   useEffect(() => {
     dispatch({
       type: SET_COMPANY_NAME,
       payload: params.companyName,
-    })
-    dispatch(getCompanyJobsAction())
-  }, [])
+    });
+    dispatch(getCompanyJobsAction());
+  }, []);
 
   return (
     <Container>
       <Row>
         <Col>
+          <Button variant="light" onClick={() => navigate("/")}>
+            Home Page
+          </Button>
           <h1>{companyName} Jobs</h1>
           {jobsFetched.map((jobData) => (
             <Job key={jobData._id} data={jobData} />
@@ -30,7 +34,7 @@ const CompanySearchResults = () => {
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default CompanySearchResults
+export default CompanySearchResults;
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,14 +1,36 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import favsReducer from '../reducers/favsReducer';
-import jobsReducer from '../reducers/jobsReducer';
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+import favsReducer from "../reducers/favsReducer";
+import jobsReducer from "../reducers/jobsReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+
+
+const persistConfig = {
+  key: "root", // il livello da cui vogliamo cominciare a far persistere i dati
+  storage, // come dire --> storage: storage, seleziona lo storage engine da utilizzare
+  transforms: [
+    // elenco delle trasformazioni (plugin) applicabili a redux-persist
+    encryptTransform({
+      secretKey: process.env.REACT_APP_PERSIST_KEY,
+    }),
+  ],
+};
 
 const generalReducer = combineReducers({
   jobs: jobsReducer,
-  favorites: favsReducer
-})
+  favorites: favsReducer,
+});
 
-const store = configureStore({
-  reducer: generalReducer
-})
+const persistedReducer = persistReducer(persistConfig, generalReducer);
 
-export default store;
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // <-- lo spegne
+    })
+});
+
+export const persistor = persistStore(store);
